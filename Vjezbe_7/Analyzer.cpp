7814c5abe680d6cab69caa68790fc07ceebca91d
mmatkovic2 <mmatkovic@pmfst.hr>
2021-12-02 12:55:25 +0100

    Zadatak 1 rijesen

--- Vjezbe_7/Analyzer.cpp
+++ Vjezbe_7/Analyzer.cpp
@@ -47,93 +47,42 @@ void Analyzer::Loop()
    }
 }
 
-void Analyzer::PlotHistogram(){
-	/*
-	Zadatak2
-	TCanvas* c = new TCanvas("c", "c", 1800, 1800);
-	c->Divide(2, 2);
-
-	TH1F* h1 = new TH1F("h1", "LepP1", 100, 0, 150);
-	TH1F* h2 = new TH1F("h2", "LepP2", 100, 0, 150);
-	TH1F* h3 = new TH1F("h3", "LepP3", 100, 0, 150);
-	TH1F* h4 = new TH1F("h4", "LepP4", 100, 0, 150);
+void Analyzer::PlotHistogram(TString name){
+	//zadatak 1
 	
-
-	if (fChain == 0)
-		return;
-	Long64_t nentries = fChain->GetEntriesFast();
-	Long64_t nbytes = 0, nb = 0;
-	for (Long64_t jentry=0; jentry<nentries;jentry++)
-	{
-		Long64_t ientry = LoadTree(jentry);
-		if (ientry < 0)
-			break;
-		nb = fChain->GetEntry(jentry);
-		nbytes += nb;
-		// if (Cut(ientry) < 0) continue;
-		
-		
-		Zadatak2
-		h1->Fill(LepPt->at(2));
-		h2->Fill(LepPt->at(3));
-		h3->Fill(LepPt->at(0));
-		h4->Fill(LepPt->at(1));
-		
-	}
-
-	//prvi od cetiri kanvasa, cd(1), gornji lijevi, p/noe
-	c->cd(1);
-
-	h1->GetXaxis()->SetTitle("p_{T} (GeV/c)");
-	h1->GetYaxis()->SetTitle("number of events");
-	//h1->SetFillColor(kBlue);
-	h1->SetStats(0);
-	h1->SetLineColor(kBlue);
-	h1->Draw();
-
-	h2->SetLineColor(kRed);
-	h2->SetLineWidth(2);
-	h2->Draw("hist same");
+	//dio kopiran iz h filea da se moze lakse mijenjat path
+	TTree *tree = new TTree;
 	
-	h3->SetLineColor(kGreen);
-	h3->SetLineWidth(2);
-	h3->Draw("hist same");
+	TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/home/public/data/"+name+"/ZZ4lAnalysis.root");
+	if (!f || !f->IsOpen())
+		f = new TFile("/home/public/data/"+name+"/ZZ4lAnalysis.root");
+	TDirectory * dir = (TDirectory*)f->Get("/home/public/data/"+name+"/ZZ4lAnalysis.root:/ZZTree");
+	dir->GetObject("candTree",tree);
+	Init(tree);
 	
-	h4->SetLineColor(kBlue);
-	h4->SetLineWidth(2);
-	h4->Draw("hist same");
-
-	//svaki kanvas ima 4 velicine, h1-h4
-
-	//postavljanje legende
-	TLegend* legend = new TLegend(0.6,0.8,0.9,0.9);
-	legend->SetTextSize(0.03);
-	legend->AddEntry(h1,"Decay lepton 1","f");
-	legend->AddEntry(h2,"Decay lepton 2","l");
-	legend->AddEntry(h3,"Decay lepton 3","l");
-	legend->AddEntry(h4,"Decay lepton 4","l");
-	legend->Draw();
+	//crtamo dva odvojena platna, kopirani kod iz vj6zad2
+	TCanvas* c = new TCanvas("c", "c", 2400, 1800);
+	c->Divide(2,2);
 	
-	c->SaveAs("LeptPt.png");
-	*/
+	TH1F* histo11 = new TH1F("histo11", "Transversal Momentum", 200, 0, 150);
+	TH1F* histo12 = new TH1F("histo12", "", 200, 0, 150);
+	TH1F* histo13 = new TH1F("histo13", "", 200, 0, 150);
+	TH1F* histo14 = new TH1F("histo14", "", 200, 0, 150);
 
-	//Zadatak3
-	//stvaramo 4 leptona klase lorentzvektor, 2 zbozona koja nastaju od 4 leptona i 1 Higgs produkt 2z
-	TLorentzVector* lep1 = new TLorentzVector();
-	TLorentzVector* lep2 = new TLorentzVector();
-	TLorentzVector* lep3 = new TLorentzVector();
-	TLorentzVector* lep4 = new TLorentzVector();
-	TLorentzVector* Z1 = new TLorentzVector();
-	TLorentzVector* Z2 = new TLorentzVector();
-	TLorentzVector* Higgs = new TLorentzVector();
-	
-	TCanvas* c = new TCanvas("c", "c", 900, 900);
-	TH1F* h1 = new TH1F("h1", "Reconstructed mass from 4 leptons", 50, 90.0, 140.0);
+	TH1F* histo21 = new TH1F("histo21", "Pseudorapidity", 100, -5, 5);
+	TH1F* histo22 = new TH1F("histo22", "", 100, -5, 5);
+	TH1F* histo23 = new TH1F("histo23", "", 100, -5, 5);
+	TH1F* histo24 = new TH1F("histo24", "", 100, -5, 5);
 
-	//zadatak 4 - ucitavanje sume svih tezina (bin content) sa ser
-	TFile f("/home/public/data/ggH125/ZZ4lAnalysis.root"); 
-	TH1F* histoCounter = (TH1F*)f.Get("ZZTree/Counters");
-	double w, contentbin;
+	TH1F* histo31 = new TH1F("histo31", "Azimuth Angle", 100, -5, 5);
+	TH1F* histo32 = new TH1F("histo32", "", 100, -5, 5);
+	TH1F* histo33 = new TH1F("histo33", "", 100, -5, 5);
+	TH1F* histo34 = new TH1F("histo34", "", 100, -5, 5);
+
+	TH1F* histo41 = new TH1F("histo41", "Boosted Decision Tree", 40, 0, 10);
+	TH1F* histo42 = new TH1F("histo42", "", 40, 0, 10);
+	TH1F* histo43 = new TH1F("histo43", "", 40, 0, 10);
+	TH1F* histo44 = new TH1F("histo44", "", 40, 0, 10);
 	
 	if (fChain == 0)
 		return;
@@ -148,36 +97,153 @@ void Analyzer::PlotHistogram(){
 		nbytes += nb;
 		// if (Cut(ientry) < 0) continue;
 		
-		lep1->SetPtEtaPhiM(LepPt->at(0), LepEta->at(0), LepPhi->at(0), 0.0);
-		lep2->SetPtEtaPhiM(LepPt->at(1), LepEta->at(1), LepPhi->at(1), 0.0);
-		lep3->SetPtEtaPhiM(LepPt->at(2), LepEta->at(2), LepPhi->at(2), 0.0);
-		lep4->SetPtEtaPhiM(LepPt->at(3), LepEta->at(3), LepPhi->at(3), 0.0);
-			
-		*Z1 = *lep1 + *lep2;
-		*Z2 = *lep3 + *lep4;
-		*Higgs = *Z1 + *Z2;
-		
-		//zadatak4
-		contentbin = histoCounter->GetBinContent(40);
-		w = (137.0 * 1000.0 * xsec * overallEventWeight) / contentbin;
-		h1->Fill(Higgs->M(),w);
+		histo11->Fill(LepPt->at(2));
+		histo12->Fill(LepPt->at(3));
+		histo13->Fill(LepPt->at(0));
+		histo14->Fill(LepPt->at(1));
+
+		histo21->Fill(LepEta->at(2));
+		histo22->Fill(LepEta->at(3));
+		histo23->Fill(LepEta->at(0));
+		histo24->Fill(LepEta->at(1));
+
+		histo31->Fill(LepPhi->at(2));
+		histo32->Fill(LepPhi->at(3));
+		histo33->Fill(LepPhi->at(0));
+		histo34->Fill(LepPhi->at(1));
+
+		histo41->Fill(LepBDT->at(2));
+		histo42->Fill(LepBDT->at(3));
+		histo43->Fill(LepBDT->at(0));
+		histo44->Fill(LepBDT->at(1));
 	}
 	
-	//plotanje
+	c->cd(1);		// LepPt //----------------------------------------------------
+	
 	gPad->SetLeftMargin(0.15);
 	
-	h1->GetXaxis()->SetTitle("m_{4l} (GeV)");
-	h1->GetYaxis()->SetTitle("no. of events");
-	h1->SetStats(0);
-	h1->GetXaxis()->SetRangeUser(90, 140);
-	h1->SetLineColor(kAzure);
-	h1->SetFillColor(kAzure);
-	h1->Draw("HIST");
-
-	//integral za zad 4
-	cout << h1->Integral(1, 50) << endl;
-
+	histo11->GetXaxis()->SetTitle("p_{T} (GeV/c)");
+	histo11->GetYaxis()->SetTitle("no. of events");
+	histo11->SetStats(0);
+	histo11->SetLineColor(kGreen);
+	histo11->SetLineWidth(4);
+	histo11->Draw();
 	
-	c->SaveAs("Z4.png");
+	histo12->SetLineColor(kRed);
+	histo12->SetLineWidth(2);
+	histo12->Draw("hist same");
+	
+	histo13->SetLineColor(kOrange);
+	histo13->SetLineWidth(4);
+	histo13->Draw("hist same");
+	
+	histo14->SetLineColor(kBlue);
+	histo14->SetLineWidth(2);
+	histo14->Draw("hist same");
+	
+	TLegend* legend1 = new TLegend(0.7,0.7,0.9,0.9);
+	legend1->SetTextSize(0.03);
+	legend1->AddEntry(histo11,"Decay lepton 1","l");
+	legend1->AddEntry(histo12,"Decay lepton 2","l");
+	legend1->AddEntry(histo13,"Decay lepton 3","l");
+	legend1->AddEntry(histo14,"Decay lepton 4","l");
+	legend1->Draw();
 
+	c->cd(2);		// LepEta //----------------------------------------------------
+	
+	gPad->SetLeftMargin(0.15);
+	
+	histo21->GetXaxis()->SetTitle("#eta");
+	histo21->GetYaxis()->SetTitle("no. of events");
+	histo21->SetStats(0);
+	histo21->SetLineColor(kGreen);
+	histo21->SetLineWidth(4);
+	histo21->Draw();
+	
+	histo22->SetLineColor(kRed);
+	histo22->SetLineWidth(2);
+	histo22->Draw("hist same");
+	
+	histo23->SetLineColor(kOrange);
+	histo23->SetLineWidth(4);
+	histo23->Draw("hist same");
+	
+	histo24->SetLineColor(kBlue);
+	histo24->SetLineWidth(2);
+	histo24->Draw("hist same");
+	
+	TLegend* legend2 = new TLegend(0.7,0.7,0.9,0.9);
+	legend2->SetTextSize(0.03);
+	legend2->AddEntry(histo21,"Decay lepton 1","l");
+	legend2->AddEntry(histo22,"Decay lepton 2","l");
+	legend2->AddEntry(histo23,"Decay lepton 3","l");
+	legend2->AddEntry(histo24,"Decay lepton 4","l");
+	legend2->Draw();
+	
+	c->cd(3);		// LepPhi //----------------------------------------------------
+	
+	gPad->SetLeftMargin(0.15);
+	
+	histo31->GetXaxis()->SetTitle("#phi (rad)");
+	histo31->GetYaxis()->SetTitle("no. of events");
+	histo31->SetStats(0);
+	histo31->SetLineColor(kGreen);
+	histo31->SetLineWidth(4);
+	histo31->Draw();
+	
+	histo32->SetLineColor(kRed);
+	histo32->SetLineWidth(2);
+	histo32->Draw("hist same");
+	
+	histo33->SetLineColor(kOrange);
+	histo33->SetLineWidth(4);
+	histo33->Draw("hist same");
+	
+	histo34->SetLineColor(kBlue);
+	histo34->SetLineWidth(2);
+	histo34->Draw("hist same");
+	
+	TLegend* legend3 = new TLegend(0.7,0.7,0.9,0.9);
+	legend3->SetTextSize(0.03);
+	legend3->AddEntry(histo31,"Decay lepton 1","l");
+	legend3->AddEntry(histo32,"Decay lepton 2","l");
+	legend3->AddEntry(histo33,"Decay lepton 3","l");
+	legend3->AddEntry(histo34,"Decay lepton 4","l");
+	legend3->Draw();
+	
+	c->cd(4);		// LepBDT //----------------------------------------------------
+	
+	gPad->SetLeftMargin(0.15);
+
+	histo41->GetXaxis()->SetTitle("BDT");
+	histo41->GetYaxis()->SetTitle("no. of events");
+	histo41->SetStats(0);
+	histo41->SetLineColor(kGreen);
+	histo41->SetLineWidth(4);
+	histo41->Draw();
+	
+	histo42->SetLineColor(kRed);
+	histo42->SetLineWidth(2);
+	histo42->Draw("hist same");
+	
+	histo43->SetLineColor(kOrange);
+	histo43->SetLineWidth(4);
+	histo43->Draw("hist same");
+	
+	histo44->SetLineColor(kBlue);
+	histo44->SetLineWidth(2);
+	histo44->Draw("hist same");
+	
+	TLegend* legend4 = new TLegend(0.7,0.7,0.9,0.9);
+	legend4->SetTextSize(0.03);
+	legend4->AddEntry(histo41,"Decay lepton 1","l");
+	legend4->AddEntry(histo42,"Decay lepton 2","l");
+	legend4->AddEntry(histo43,"Decay lepton 3","l");
+	legend4->AddEntry(histo44,"Decay lepton 4","l");
+	legend4->Draw();
+	
+	c->SaveAs("Leptons_"+name+".png");
+	
+	
+	
 }
---
+++ Vjezbe_7/analyze.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TApplication.h>
+#include <Analyzer.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{	
+	Analyzer* ob;
+	ob = new Analyzer();
+	
+	ob->PlotHistogram("ggH125");
+	ob->PlotHistogram("qqZZ");
+	
+	delete ob;
+	
+	return 0;
+}
